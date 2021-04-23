9c55ef57c7aa26e9c629f85f6436c109045ff643
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-04-23 17:00:12 +0900

    13번

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -3,6 +3,7 @@ package kr.ac.jejunu.userdao;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
@@ -22,12 +23,12 @@ public class DaoFactory {
 
     @Bean
     public UserDao userDao() {
-        return new UserDao(jdbcContext());
+        return new UserDao(jdbcTemplate());
     }
 
     @Bean
-    public JdbcContext jdbcContext(){
-        return new JdbcContext(getConnection());
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(getConnection());
     }
 
     @Bean
